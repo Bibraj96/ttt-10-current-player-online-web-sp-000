3c88a08e5070c4d71b649b8cea6f891c824e65ee
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-18 19:26:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,8 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board) = turns
-  if turns.even? == true
+   if turn_count(board).even? == true
     puts "X"
   else
     puts "O"
